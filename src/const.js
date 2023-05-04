83c9d1dc999b057ab83802eb9ac261a78d448e9d
vorthkor <31862750+vorthkor@users.noreply.github.com>
2023-05-04 15:20:37 -0300

    fix: local port to 3333

--- src/const.js
+++ src/const.js
@@ -1,6 +1,9 @@
-const VUE_APP_SFINGERS_API = "http://localhost:3344"
+const VUE_APP_SFINGERS_API = "http://localhost:3333"
 
 const apiSfingersBase = VUE_APP_SFINGERS_API
+
+
+console.log(apiSfingersBase)
 
 export const apiSfingers = {
     Products: apiSfingersBase + "/products",
